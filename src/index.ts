5f3b7be74e4d8a2b4824259c4c4c70c489db0fe2
ktomkow <krzysztof.tomkow@gmail.com>
2023-02-19 17:48:04 +0100

    V4

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-'use strict';
-
-Object.defineProperty(exports, '__esModule', { value: true });
-
-const main_module = require('./main');
-const response_module = require('./response');
-
-exports.math = main_module.math;
-exports.pow = response_module.pow;
+export * from './';
