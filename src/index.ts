27fd6e4ce3653355825772f89aef0208873ab676
ktomkow <krzysztof.tomkow@gmail.com>
2023-02-19 17:44:22 +0100

    Work on publication

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export * from "./main";
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const main_module = require('./main');
+const response_module = require('./response');
+
+exports.math = main_module.math;
+exports.pow = response_module.pow;
